e807cd4c01a3886f226f837940a8838a8ee6c569
Bhavik Barot <39043956+BhavikBarot@users.noreply.github.com>
2022-05-25 14:04:49 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,11 +10,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BHAPIManager",
-            targets: ["BHAPIManager"]),
+            targets: ["BHAPIManager"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,6 +22,6 @@ let package = Package(
         .binaryTarget(name: "BHAPIManager", path: "Sources/Frameworks/BHAPIManager.xcframework"),
         .testTarget(
             name: "BHAPIManagerTests",
-            dependencies: ["BHAPIManager"]),
+            dependencies: ["BHAPIManager"])
     ]
 )
